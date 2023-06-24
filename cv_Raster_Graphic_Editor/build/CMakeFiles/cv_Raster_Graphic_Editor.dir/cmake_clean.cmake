e813df2ac86523969430fc6271be4b033289d884
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-06-23 20:12:27 -0500

    RGE - EYEDROPPER, Toogle b/w Tools working

---
+++ cv_Raster_Graphic_Editor/build/CMakeFiles/cv_Raster_Graphic_Editor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_Raster_Graphic_Editor.dir/cv_Raster_Graphic_Editor.cpp.o"
+  "CMakeFiles/cv_Raster_Graphic_Editor.dir/cv_Raster_Graphic_Editor.cpp.o.d"
+  "cv_Raster_Graphic_Editor"
+  "cv_Raster_Graphic_Editor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_Raster_Graphic_Editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
