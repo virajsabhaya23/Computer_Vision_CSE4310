64c917e5ad16d21aea8f03bcc9b8b3b7d665674f
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-06-23 22:35:55 -0500

    CROP, PENCIL, PAINT_BUCKET working

---
+++ cv_Raster_Graphic_Editor/build/CMakeFiles/cv_Raster_Graphic_Editor.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/virajsabhaya/Library/CloudStorage/OneDrive-UniversityofTexasatArlington/Documents/UTA SEM #7.5 SUM 2023/CSE 4310-001 ComVision/Computer_Vision_CSE4310/cv_Raster_Graphic_Editor/cv_Raster_Graphic_Editor.cpp" "CMakeFiles/cv_Raster_Graphic_Editor.dir/cv_Raster_Graphic_Editor.cpp.o" "gcc" "CMakeFiles/cv_Raster_Graphic_Editor.dir/cv_Raster_Graphic_Editor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cv_Raster_Graphic_Editor/build/CMakeFiles/cv_Raster_Graphic_Editor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cv_Raster_Graphic_Editor.
+# This may be replaced when dependencies are built.
