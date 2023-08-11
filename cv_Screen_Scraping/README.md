4cfd986075f904363e7e464cbbb0c6841eb942af
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-08-11 02:08:48 -0500

    Screen Scraping base code

---
+++ cv_Screen_Scraping/README.md
@@ -0,0 +1,22 @@
+# Computer_Vision_CSE4310
+
+---
+
+## Project 5: Screen Scraping
+
+- The program should be able to be compiled and executed by running the following set of commands in the program directory:
+
+```bash
+mkdir build && cd build
+cmake ..
+make
+./cv_Screen_Scraping ../screen_scrape.mp4
+```
+or
+```bash
+cmake .
+make
+./cv_Screen_Scraping screen_scrape.mp4
+```
+---
+
---
+++ cv_Screen_Scraping/cv_Screen_Scraping.cpp
@@ -0,0 +1,115 @@
+//
+//    Copyright 2023 Viraj V. Sabhaya
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/*******************************************************************************************************************/ /**
+ * @file cv_Traffic_Counter.cpp
+ * @brief C++ Screen Scraping example for OpenCV
+ * @author Viraj V. Sabhaya
+**********************************************************************************************************************/
+
+// include necessary dependencies
+#include <iostream>
+#include <cstdio>
+#include "opencv2/opencv.hpp"
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+
+// Global variables
+using namespace std;
+using namespace cv;
+
+Scalar GREEN_COLOR(0, 255, 0);
+Scalar RED_COLOR(0, 0, 255); 
+Scalar BLUE_COLOR(255, 0, 0);
+Scalar YELLOW_COLOR(0, 255, 255);
+
+// configuration parameters
+#define NUM_COMMAND_LINE_ARGUMENTS 1
+
+/*******************************************************************************************************************/ /**
+ * @brief program entry point
+ * @param[in] argc number of command line arguments
+ * @param[in] argv string array of command line arguments
+ * @return return code (0 for normal termination)
+ * @author Viraj V. Sabhaya
+**********************************************************************************************************************/
+int main(int argc, char *argv[])
+{
+    string videoFileName;
+
+    if (argc != NUM_COMMAND_LINE_ARGUMENTS + 1)
+    {
+        printf("Usage: %s <video_file>\n", argv[0]);
+        return 0;
+    }
+    else
+    {
+        videoFileName = argv[1];
+    }
+
+    // open video file
+    VideoCapture capture(videoFileName);
+    if (!capture.isOpened())
+    {
+        cout << "Unable to open video source, terminating program!" << endl;
+        return 0;
+    }
+    
+    int captureWidth = capture.get(CAP_PROP_FRAME_WIDTH);
+    int captureHeight = capture.get(CAP_PROP_FRAME_HEIGHT);
+    int captureFPS = capture.get(CAP_PROP_FPS);
+    cout << "Video source opened successfully!" << endl;
+    cout << "Width: " << captureWidth << endl;
+    cout << "Height: " << captureHeight << endl;
+    cout << "FPS: " << captureFPS << endl;
+
+    // created displaying windows
+    namedWindow("capturedFrame", WINDOW_AUTOSIZE);
+
+
+    bool tracking = true;
+
+    while(tracking)
+    {
+        Mat capturedFrame;
+
+        // read frame from video source
+        bool captureSuccess = capture.read(capturedFrame);
+
+        // updating GUI window
+        if (captureSuccess)
+        {
+            imshow("capturedFrame", capturedFrame);
+        }
+        else
+        {
+            cout << "Video playback finished! " << endl;
+            tracking = false;
+            continue;
+        }
+
+
+        if(((char)waitKey(1) == 'q'))
+        {
+            tracking = false;
+        }
+    }
+
+    // releasing the captured video and destryoing all windows
+    capture.release();
+    destroyAllWindows();
+}
